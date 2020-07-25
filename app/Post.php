1a65b70bdf9ded1e3ef93431e56ed3edf29cca46
Bipin karki <bipinkarki172@gmail.com>
2020-07-25 13:35:27 +0545

    Add tags/post/comments pages

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    
     protected $fillable = ['title','content','post_type','user_id',
     'category_id', 'meta_data'];
 
